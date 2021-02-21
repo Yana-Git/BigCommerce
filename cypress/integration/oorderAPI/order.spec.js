61817024c533121f3e1932f2e5d8c219f13bf7fa
Wang Yao <yaowang274@gmail.com>
2021-02-21 20:51:50 +1300

    。。。

--- cypress/integration/oorderAPI/order.spec.js
+++ cypress/integration/oorderAPI/order.spec.js
@@ -1,4 +1,5 @@
 describe("order API test", function () {
+  let id = 0;
   it("create an order", function () {
     cy.request({
       method: "post",
@@ -32,33 +33,32 @@ describe("order API test", function () {
       },
     }).then((response) => {
       expect(response.status).equal(201);
+      id = response.body.id;
     });
   });
 
-  it("check new order", function () {
-    cy.visit("https://login.bigcommerce.com/login");
-    cy.get("#user_email").clear().type("yaowang274@gmail.com");
-    cy.get("#user_password").clear().type("wangSONG123.");
-    cy.get(".login-form-button").click();
-    cy.url().should("include", "/manage/dashboard");
-    cy.get("a").find("span").contains("Orders").click();
-    cy.get(".order-status");
-  });
+  // it("check new order", function () {
+  //   cy.visit("https://login.bigcommerce.com/login");
+  //   cy.get("#user_email").clear().type("yaowang274@gmail.com");
+  //   cy.get("#user_password").clear().type("wangSONG123.");
+  //   cy.get(".login-form-button").click();
+  //   cy.url().should("include", "/manage/dashboard");
+  //   cy.get("a").find("span").contains("Orders").click();
+  //   cy.get(".order-status");
+  // });
 
-  it.only("check order status", function () {
+  it("check order status", function () {
     cy.request({
       method: "get",
-      url: "https://api.bigcommerce.com/stores/747hf8df3w/v3/catalog/summary",
+      url: `https://api.bigcommerce.com/stores/747hf8df3w/v2/orders/${id}`,
       headers: {
         "X-Auth-Token": "kcylj96oghf7q8lzp215t5vkcl4f11u",
         "Content-Type": "application/json",
         Accept: "application/json",
       },
-      body: {
-        order_id: "105",
-      },
     }).then((response) => {
       expect(response.status).equal(200);
+      expect(response.body.custom_status).equal("Awaiting Fulfillment");
     });
   });
   after(function () {
