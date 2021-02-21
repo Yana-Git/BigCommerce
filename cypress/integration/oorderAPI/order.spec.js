e6f8a83669b9303f72a1811cff1171b1ded8e63c
Wang Yao <yaowang274@gmail.com>
2021-02-21 16:11:18 +1300

    checked order status and cleared test data

--- cypress/integration/oorderAPI/order.spec.js
+++ cypress/integration/oorderAPI/order.spec.js
@@ -1,5 +1,5 @@
 describe("order API test", function () {
-  it.only("create an order", function () {
+  it("create an order", function () {
     cy.request({
       method: "post",
       url: "https://api.bigcommerce.com/stores/747hf8df3w/v2/orders",
@@ -28,11 +28,13 @@ describe("order API test", function () {
             price_ex_tax: 45,
           },
         ],
+        status_id: 11,
       },
     }).then((response) => {
       expect(response.status).equal(201);
     });
   });
+
   it("check new order", function () {
     cy.visit("https://login.bigcommerce.com/login");
     cy.get("#user_email").clear().type("yaowang274@gmail.com");
@@ -41,5 +43,35 @@ describe("order API test", function () {
     cy.url().should("include", "/manage/dashboard");
     cy.get("a").find("span").contains("Orders").click();
     cy.get(".order-status");
+  });
+
+  it.only("check order status", function () {
+    cy.request({
+      method: "get",
+      url: "https://api.bigcommerce.com/stores/747hf8df3w/v3/catalog/summary",
+      headers: {
+        "X-Auth-Token": "kcylj96oghf7q8lzp215t5vkcl4f11u",
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: {
+        order_id: "105",
+      },
+    }).then((response) => {
+      expect(response.status).equal(200);
+    });
+  });
+  after(function () {
+    cy.request({
+      method: "delete",
+      url: "https://api.bigcommerce.com/stores/747hf8df3w/v2/orders",
+      headers: {
+        "X-Auth-Token": "kcylj96oghf7q8lzp215t5vkcl4f11u",
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    }).then((response) => {
+      expect(response.status).equal(204);
+    });
   });
 });
